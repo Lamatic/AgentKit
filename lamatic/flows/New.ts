9a2b43f8f66450abee80eb0c604c1b68320dbb63
Darshan S <darshans2727@gmail.com>
2026-03-20 13:49:01 +0530

    [LAMATIC-COMMIT] Updated flow

--- lamatic/flows/New.ts
+++ lamatic/flows/New.ts
@@ -4,15 +4,102 @@ const flowConfig = {
   "nodes": [
     {
       "id": "triggerNode_1",
-      "data": {},
+      "data": {
+        "modes": {},
+        "nodeId": "chatTriggerNode",
+        "values": {
+          "id": "triggerNode_1",
+          "chat": "",
+          "domains": [
+            "*"
+          ],
+          "nodeName": "Chat Widget",
+          "chatConfig": {
+            "botName": "Lamatic Bot",
+            "imageUrl": "https://img.freepik.com/premium-vector/robot-android-super-hero_111928-7.jpg?w=826",
+            "position": "right",
+            "policyUrl": "https://lamatic.ai/docs/legal/privacy-policy",
+            "displayMode": "popup",
+            "placeholder": "Compose your message",
+            "suggestions": [
+              "What is lamatic?",
+              "How do I add data to my chatbot?",
+              "Explain this product to me"
+            ],
+            "errorMessage": "Oops! Something went wrong. Please try again.",
+            "hideBranding": false,
+            "primaryColor": "#ef4444",
+            "headerBgColor": "#000000",
+            "greetingMessage": "Hi, I am Lamatic Bot. Ask me anything about Lamatic",
+            "headerTextColor": "#FFFFFF",
+            "showEmojiButton": true,
+            "suggestionBgColor": "#f1f5f9",
+            "userMessageBgColor": "#FEF2F2",
+            "agentMessageBgColor": "#f1f5f9",
+            "suggestionTextColor": "#334155",
+            "userMessageTextColor": "#d12323",
+            "agentMessageTextColor": "#334155"
+          }
+        },
+        "trigger": true
+      },
       "type": "triggerNode",
+      "measured": {
+        "width": 216,
+        "height": 93
+      },
       "position": {
         "x": 0,
         "y": 0
-      }
+      },
+      "selected": false
+    },
+    {
+      "id": "responseNode_triggerNode_1",
+      "data": {
+        "label": "Response",
+        "nodeId": "chatResponseNode",
+        "values": {
+          "id": "responseNode_triggerNode_1",
+          "content": "Respond to the question",
+          "nodeName": "Chat Response",
+          "references": "",
+          "webhookUrl": "",
+          "webhookHeaders": ""
+        },
+        "disabled": false,
+        "isResponseNode": true
+      },
+      "type": "responseNode",
+      "measured": {
+        "width": 216,
+        "height": 93
+      },
+      "position": {
+        "x": 0,
+        "y": 130
+      },
+      "selected": true
     }
   ],
-  "edges": [],
+  "edges": [
+    {
+      "id": "triggerNode_1-responseNode_triggerNode_1",
+      "type": "defaultEdge",
+      "source": "triggerNode_1",
+      "target": "responseNode_triggerNode_1",
+      "sourceHandle": "bottom",
+      "targetHandle": "top"
+    },
+    {
+      "id": "response-trigger_triggerNode_1",
+      "type": "responseEdge",
+      "source": "triggerNode_1",
+      "target": "responseNode_triggerNode_1",
+      "sourceHandle": "to-response",
+      "targetHandle": "from-trigger"
+    }
+  ],
   "status": "active",
   "created_at": "2026-03-20T04:45:17.065716+00:00"
 };
