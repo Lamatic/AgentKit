0332c56ecc1d5441aec9b2ab2abfd4c5172e4f57
Darshan S <darshans2727@gmail.com>
2026-03-20 10:15:18 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/New.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "b29ba3fc-3367-4c60-8e5c-8dfee11b168d",
+  "name": "New",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2026-03-20T04:45:17.065716+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
