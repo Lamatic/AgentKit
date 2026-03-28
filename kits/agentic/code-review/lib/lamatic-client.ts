2ed35e92bacb7b48232b077a2661d79d7c64dbd0
Soumik15630m <soumik15630m@gmail.com>
2026-03-28 11:36:16 +0530

    Disable check for AGENTIC_GENERATE_CONTENT
    Comment out error check for AGENTIC_GENERATE_CONTENT.

--- kits/agentic/code-review/lib/lamatic-client.ts
+++ kits/agentic/code-review/lib/lamatic-client.ts
@@ -1,11 +1,11 @@
 import { Lamatic } from "lamatic";
 import {config} from '../orchestrate.js'
 
-if (!process.env.AGENTIC_GENERATE_CONTENT) {
-  throw new Error(
-    "All Workflow IDs in environment variable are not set. Please add it to your .env.local file."
-  );
-}
+// if (!process.env.AGENTIC_GENERATE_CONTENT) {
+//   throw new Error(
+//     "All Workflow IDs in environment variable are not set. Please add it to your .env.local file."
+//   );
+// }
 
 if (!process.env.LAMATIC_API_URL || !process.env.LAMATIC_PROJECT_ID || !process.env.LAMATIC_API_KEY) {
   throw new Error(
