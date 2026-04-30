8b952a4dbf38fc74a75afb020d84d020ae68d62d
yashasvij-19 <yashasvivij01@gmail.com>
2026-04-30 15:56:53 +0530

    FIX: update lamatic.config.ts

--- kits/fablens/lamatic.config.ts
+++ kits/fablens/lamatic.config.ts
@@ -1,14 +1,15 @@
 export default {
-  name: "fablens",
+  name: "fabric-material-analyzer",
   description: "Analyzes clothing product URLs to identify materials and provide factual environmental and skin safety insights.",
   version: "1.0.0",
   type: "kit" as const,
   author: { name: "Yashasvi", email: "yashasvivij01@@email.com" },
   tags: ["sustainability", "fashion", "environment", "skin-safety", "consumer"],
   steps: [
-    { id: "fabric-material-analyzer", type: "mandatory" as const, envKey: "FABLENS_WORKFLOW_ID" }
+    { id: "fabric-material-analyzer", type: "kit" as const, envKey: "FABLENS_WORKFLOW_ID" }
   ],
   links: {
-    github: "https://github.com/yashasvij-19/AgentKit/tree/main/kits/fablens",
+    github: "https://github.com/yashasvij-19/AgentKit/tree/new-main/kits/fablens",
+    deploy:"https://vercel.com/new/clone?kits/fablens/apps"
   }
 };
