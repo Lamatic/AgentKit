c3e494a3d3565272ba600cf5964603e16c33f3bf
TiyaJain28 <tiyajain28102005@gmail.com>
2026-05-28 17:47:30 +0530

    Add files via upload

---
+++ kits/meeting-preparation-agent/lamatic.config.ts
@@ -0,0 +1,21 @@
+export default {
+  "name": "meeting-preparation-agent",
+  "description": "",
+  "version": "1.0.0",
+  "type": "kit",
+  "author": {
+    "name": "tiyajain28102005",
+    "email": "tiyajain28102005@gmail.com"
+  },
+  "tags": [],
+  "steps": [
+    {
+      "id": "meeting-preparation-agent",
+      "type": "mandatory"
+    }
+  ],
+  "links": {
+    "deploy": "",
+    "github": ""
+  }
+};
