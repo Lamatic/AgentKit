217183d5b27c53822c28ecb5176e0ff6d90b42d5
Rutvija Mali <127505171+rutvija-mali@users.noreply.github.com>
2026-05-31 00:24:54 +0530

    Add files via upload

---
+++ kits/fashion-outfit-analyzer/agent.md
@@ -0,0 +1,3 @@
+# fashion-outfit-analyzer
+
+<!-- TODO: Add agent overview, purpose, flow descriptions, guardrails, and integration reference -->
---
+++ kits/fashion-outfit-analyzer/lamatic.config.ts
@@ -0,0 +1,21 @@
+export default {
+  "name": "fashion-outfit-analyzer",
+  "description": "",
+  "version": "1.0.0",
+  "type": "kit",
+  "author": {
+    "name": "Rutvija Mali",
+    "email": "rutvijamali@gmail.com"
+  },
+  "tags": [],
+  "steps": [
+    {
+      "id": "fashion-outfit-analyzer",
+      "type": "mandatory"
+    }
+  ],
+  "links": {
+    "deploy": "",
+    "github": ""
+  }
+};
