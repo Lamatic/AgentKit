5baae1469e3b67f85b1bc406253b17aa5f2f6137
Harshul Dashora <160769697+harshuldashora@users.noreply.github.com>
2026-06-05 21:04:56 +0530

    Create lamatic.config.ts

---
+++ kits/ai-interview-preparation-agent/lamatic.config.ts
@@ -0,0 +1,16 @@
+export default {
+  "name": "resume-parser",
+  "description": "An AI-powered resume parser that extracts key information such as personal details, skills, education, work experience, and projects from resumes, converting unstructured documents into structured data for efficient candidate screening and analysis.",
+  "version": "1.0.0",
+  "type": "kit",
+  "author": {
+    "name": "Harshul Dashora",
+    "email": "harshuldashora01@gmail.com"
+  },
+  "steps": [
+    {
+      "id": "resume-parser",
+      "type": "mandatory"
+    }
+  ]
+};
