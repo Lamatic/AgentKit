7b4ca22ab7e4ba9454a566dd41f450088c921c4f
Yash Gupta <yggupta9414@gmail.com>
2026-07-07 18:10:57 +0530

    feat: add learning path replanner template

---
+++ kits/learning-path-replanner/model-configs/learning-path-replanner_llmnode-593_generative-model-name.ts
@@ -0,0 +1,15 @@
+// Model config: llmnode-593 (LLMNode)
+
+export default {
+  "generativeModelName": [
+    {
+      "type": "generator/text",
+      "params": {},
+      "configName": "configA",
+      "model_name": "groq/llama-3.3-70b-versatile",
+      "credentialId": "32548a6b-7257-4e98-b7bc-af545a7de462",
+      "provider_name": "groq",
+      "credential_name": "groq"
+    }
+  ]
+};
