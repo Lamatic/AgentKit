547a8a5c18e59bbdb8f27b44beaabacaf13403dc
Yash Gupta <yggupta9414@gmail.com>
2026-07-07 18:33:11 +0530

    fix: remove hard-coded Lamatic credential ids

--- kits/learning-path-replanner/model-configs/learning-path-replanner_llmnode-333_generative-model-name.ts
+++ kits/learning-path-replanner/model-configs/learning-path-replanner_llmnode-333_generative-model-name.ts
@@ -7,7 +7,7 @@ export default {
       "params": {},
       "configName": "configA",
       "model_name": "groq/llama-3.3-70b-versatile",
-      "credentialId": "32548a6b-7257-4e98-b7bc-af545a7de462",
+      "credentialId": "",
       "provider_name": "groq",
       "credential_name": "groq"
     }
