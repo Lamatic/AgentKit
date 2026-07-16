1d266b0b0d9a48a269ac8b1cbc42cfa148dca0c7
Dhanu Anurshetru <dhanuanur25@gmail.com>
2026-07-16 12:30:22 -0400

    Add files via upload

---
+++ kits/company-interview-research-agent/lamatic.config.ts
@@ -0,0 +1,21 @@
+export default {
+  "name": "company-interview-research-agent",
+  "description": "",
+  "version": "1.0.0",
+  "type": "kit",
+  "author": {
+    "name": "Dhanu Anurshetru",
+    "email": "dhanuanur25@gmail.com"
+  },
+  "tags": [],
+  "steps": [
+    {
+      "id": "company-interview-research-agent",
+      "type": "mandatory"
+    }
+  ],
+  "links": {
+    "deploy": "",
+    "github": ""
+  }
+};
