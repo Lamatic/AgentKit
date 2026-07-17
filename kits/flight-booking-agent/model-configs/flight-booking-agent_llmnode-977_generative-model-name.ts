cf7a8fe94f696fbdaf294a6bde9f97155bd12c5d
Nhlalo <nhlalonkosi@gmail.com>
2026-07-17 15:45:44 +0200

    feat: add the exported files from lamatic

---
+++ kits/flight-booking-agent/model-configs/flight-booking-agent_llmnode-977_generative-model-name.ts
@@ -0,0 +1,15 @@
+// Model config: llmnode-977 (LLMNode)
+
+export default {
+  "generativeModelName": [
+    {
+      "type": "generator/text",
+      "params": {},
+      "configName": "configA",
+      "model_name": "groq/llama-3.1-8b-instant",
+      "credentialId": "e412473b-c79d-4462-b896-5a70fcdb5055",
+      "provider_name": "groq",
+      "credential_name": "Flight Search AI"
+    }
+  ]
+};
---
+++ kits/flight-booking-agent/prompts/flight-booking-agent_llmnode-977_user_1.md
@@ -0,0 +1,1 @@
+User requested: {{triggerNode_1.output.message}}
