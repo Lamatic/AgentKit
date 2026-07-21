025229faae9e4132d17bef01255dac43ce09f7e0
Saba Fathima <sabafathima789nazar@gmail.com>
2026-07-21 15:46:17 +0530

    feat: Add mirror-check template

---
+++ kits/mirror-check/model-configs/mirror-check_instructor-llmnode-797_generative-model-name.ts
@@ -0,0 +1,15 @@
+// Model config: instructor-llmnode-797 (InstructorLLMNode)
+
+export default {
+  "generativeModelName": [
+    {
+      "type": "generator/text",
+      "params": {},
+      "configName": "configA",
+      "model_name": "groq/llama-3.3-70b-versatile",
+      "credentialId": "b09d9711-a429-4467-b242-a7940e6a2a1d",
+      "provider_name": "groq",
+      "credential_name": "Saba fathima"
+    }
+  ]
+};
