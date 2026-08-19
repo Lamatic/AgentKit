22e32b7e07752f4f60cb8f18f3eb3b622a2019a0
Manvendra Raj Singh <manvendrarajsinghmanny@gmail.com>
2026-08-19 21:31:17 +0530

    Create interview-prep-generator_generate-text.ts

---
+++ kits/interview-prep-generator/model-configs/interview-prep-generator_generate-text.ts
@@ -0,0 +1,21 @@
+// Model config: Generate Text (LLMNode)
+// Flow: interview-prep-generator
+//
+// Select a chat-capable text model in Studio when you deploy this kit -
+// credentials are per-deployer and intentionally not committed here.
+
+export default {
+    "generativeModelName": [
+      {
+              "configName": "configA",
+              "type": "generator/text",
+              "provider_name": "",
+              "credential_name": "",
+              "params": {}
+      }
+        ],
+    "credentials": "",
+    "memories": "[]",
+    "messages": "[]",
+    "attachments": ""
+};
